9bf4edbaed0142c116f68d0b2dd49029aa85f092
Ian Curran <icurran@protonmail.com>
2022-10-20 14:14:20 -0400

    Added bar padding

--- config.def.h
+++ config.def.h
@@ -7,8 +7,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "mononoki:size=12" };
-static const char dmenufont[]       = "mononoki:size=12";
+static const char dmenufont[]       = "mononoki:size=14";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
