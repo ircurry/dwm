16578f0147cef77a564457a7bd270a29916dd86f
Ian Curran <icurran@protonmail.com>
2022-11-13 11:48:38 -0500

    Bar enabled by default

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "mononoki:size=12", "Hack Nerd Font:pixelsize=38:autohint=trueanitalias=true" };
 static const char dmenufont[]       = "mononoki:size=12";
