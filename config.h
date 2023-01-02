b894ad7b42c4da2e03e6d808c180480039eef18e
Ian Curran <icurran@protonmail.com>
2023-01-02 18:09:07 -0500

    Emoji font now size not pixelsize

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Term:size=12", "Hack Nerd Font:pixelsize=38:autohint=trueanitalias=true" };
+static const char *fonts[]          = { "Iosevka Term:size=12", "Hack Nerd Font:size=14:autohint=trueanitalias=true" };
 static const char dmenufont[]       = "Iosevka Term:size=12";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -64,16 +64,17 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class        instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",       NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox",    NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
-	{ "St",         NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "Alacritty",  NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "Emacs",      NULL,     NULL,           1 << 2,    0,          0,           1,        -1 },
-	{ "Hometag",    NULL,     NULL,           1,         0,          1,           0,        -1 },
-	{ "Launchfzf",  NULL,     NULL,           0,         1,          1,           0,        -1 },
-	{ "eww",        NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ NULL,         NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */};
+	/* class         instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",        NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox",     NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
+	{ "Tor Browser", NULL,     NULL,           1 << 3,    1,          0,          -1,        -1 },
+	{ "St",          NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Alacritty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Emacs",       NULL,     NULL,           1 << 2,    0,          0,           1,        -1 },
+	{ "Hometag",     NULL,     NULL,           1,         0,          1,           0,        -1 },
+	{ "Launchfzf",   NULL,     NULL,           0,         1,          1,           0,        -1 },
+	{ "eww",         NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ NULL,          NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */};
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
